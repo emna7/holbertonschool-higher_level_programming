f0a394a22800348395385abe8fd9c9906adf5d3a
Emna <bhmemna7@gmail.com>
2019-12-19 22:24:44 +0000

    change

--- 0x04-python-more_data_structures/102-complex_delete.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    copy = my_dict.copy()
-    for y, z in copy.items():
-        if value in z:
-            del my_dict[y]
-    return my_dict
