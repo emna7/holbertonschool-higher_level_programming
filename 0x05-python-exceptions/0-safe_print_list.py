f260c9dc418d34cc26fe4534f1908ff4ac3b5769
Emna <bhmemna7@gmail.com>
2020-01-08 17:42:10 +0000

    mandatory task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        j = 0
+        for i in range(0, x):
+            print(my_list[i], end="")
+            j += 1
+    except (TypeError, IndexError):
+        pass
+    print("")
+    return j
