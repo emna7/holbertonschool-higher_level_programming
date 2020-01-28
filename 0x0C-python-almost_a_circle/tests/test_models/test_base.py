e022e92a6b8465b0e0862dc8af35de2e22531403
Emna <bhmemna7@gmail.com>
2020-01-28 22:51:47 +0000

    create

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,3 +2,20 @@
 import unittest
 import json
 from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+
+    def test_Increment(self):
+        """ tests creating a new obj increments
+        nb_objects
+        """
+        base = Base()
+        self.assertEqual(base.id, base._Base__nb_objects)
+        base2 = Base()
+        self.assertEqual(base2.id, base2._Base__nb_objects)
+    def test_IdIsString(self):
+        """ tests id as string
+        """
+        basestr = Base("String")
+        self.assertEqual(basestr.id, "String")
