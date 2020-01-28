29c0cd9d1d0e937e25331f14652cd86e8d28395b
Emna <bhmemna7@gmail.com>
2020-01-28 22:18:12 +0000

    fixinf task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,8 +24,8 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """ the to_json function """
-        if list-dictionaries is None:
-            return ("[]")
+        if list_dictionaries is None:
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
