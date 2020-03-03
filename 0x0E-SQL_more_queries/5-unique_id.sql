9214bb8ba5699e33034bf3c8bd5db1de1fc062ee
Emna <bhmemna7@gmail.com>
2020-03-03 21:14:08 +0000

    create

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- a script that creates the table unique_id on my SQL sever
+CREATE TABLE IF NOT EXISTS unique_id(id INT UNIQUE DEFAULT 1, name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,2 @@
+-- a script that lists all the cities of california that can be found in the database
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
