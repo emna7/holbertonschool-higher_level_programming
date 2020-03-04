c0ca468c2fbca0b3bbc082d14530bfc8eed5a30c
Emna <bhmemna7@gmail.com>
2020-03-04 13:13:21 +0000

    create

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-INNER JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title, genre_id ASC;
