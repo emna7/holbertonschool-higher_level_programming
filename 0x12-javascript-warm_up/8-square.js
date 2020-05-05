6c8b5e650de9da8b04dee266cb93c60aed1ac767
Emna <bhmemna7@gmail.com>
2020-05-05 20:55:07 +0000

    semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,19 +2,19 @@
 if (process.argv.length < 3) {
   console.log('Missing size');
 } else {
-const size = parseInt(process.argv[2]);
-if (isNaN(size)) {
-  console.log('Missing size');
-} else {
-let i;
-let j;
-let output = '';
-for (i = 0; i < size; i++) {
-    for (j = 0; j < size; j++) {
+  const size = parseInt(process.argv[2]);
+  if (isNaN(size)) {
+    console.log('Missing size');
+  } else {
+    let i;
+    let j;
+    let output = '';
+    for (i = 0; i < size; i++) {
+      for (j = 0; j < size; j++) {
         output += 'X';
+      }
+      console.log(output);
+      output = '';
     }
-    console.log(output);
-    output ='';
-}
-}
+  }
 }
