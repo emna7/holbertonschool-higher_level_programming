70ee6e743bc3c14460a08630abc644ea1db7fa36
Emna <bhmemna7@gmail.com>
2020-05-05 19:27:00 +0000

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function factorial (x) {
+  if (x === 0 || !Number(x)) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
+}
+console.log(factorial(Number(process.argv[2])));
