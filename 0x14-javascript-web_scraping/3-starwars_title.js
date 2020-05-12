23a27721e4485dfdbcd93af8a0f228e780ef6bd4
Emna <bhmemna7@gmail.com>
2020-05-12 19:22:09 +0000

    create

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+const process = require('process');
 const request = require('request');
 
 request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.parse(body).title);
+    console.log(JSON.parse(body)['title']);
   }
 });
