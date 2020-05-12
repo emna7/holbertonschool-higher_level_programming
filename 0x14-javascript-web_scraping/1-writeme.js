639fab0fd64914e68963a44bf94e9c88d0b6a905
Emna <bhmemna7@gmail.com>
2020-05-12 16:41:13 +0000

    fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-let path = process.argv[2];
-let data = process.argv[3];
+const path = process.argv[2];
+const data = process.argv[3];
 fs.writeFile(path, data, function (err) {
   if (err) {
     console.log(err);
