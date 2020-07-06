269fb7c1b65311da406527e2de3d6f4c13e47a96
emna7 <bhmemna7@gmail.com>
2020-07-06 22:25:41 +0100

    create

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
-const link = process.argv[2];
-const completed = {};
-request(link, function (error, response, body) {
-  if (error) throw error;
-  const dta = JSON.parse(body);
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].completed === true) {
-      if (completed[data[i].userId]) {
-        completed[data[i].userId] += 1;
-      } else {
-        completed[data[i].userId] = 1;
+const url = process.argv[2];
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    tasks.forEach(task => {
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
-    }
+    });
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
-  console.log(completed);
 });
